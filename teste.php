f05bcb3d426da4d9eb70879ca74b37babce0208d
Freddy Baier <xneerd157@gmail.com>
2022-12-14 09:40:02 -0300

    teste

--- teste.php
+++ teste.php
@@ -8,18 +8,21 @@ $headers = getallheaders();
 
 $apikey = $headers['API-Key'];
 
-if ($apikey=="123") {
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  if ($apikey=="123") {
 
-  $data = [
-    'id' => 2,
-    'name' => 'John Doe',
-    'email' => 'john.doe@example.com'
-  ];
+    $data = [
+      'id' => 2,
+      'name' => 'John Doe',
+      'email' => 'john.doe@example.com'
+    ];
 
-  echo json_encode($data);
-
+    echo json_encode($data);
+  }
+  http_response_code(401);
+  echo json_encode(array("message" => "A chave API é inválida."));
+  exit;
 }
-
 http_response_code(401);
-echo json_encode(array("message" => "A chave API é inválida."));
+echo json_encode(array("message" => "Request inválida: GET"));
 exit;
