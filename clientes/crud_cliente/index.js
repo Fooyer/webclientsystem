f65701d1da9a94aeb572abf0bcde362f8efc89b1
Freddy Baier <xneerd157@gmail.com>
2023-01-05 13:41:25 -0300

    teste

--- clientes/crud_cliente/index.js
+++ clientes/crud_cliente/index.js
@@ -2,6 +2,6 @@ import Table from "../../components/table"
 
 window.onload = function() {
 
-    const tabela = new MinhaClasse(20,'./../data_operations/obterClientes.php')
+    const tabela = new Table(20,'./../data_operations/obterClientes.php')
 
 };
