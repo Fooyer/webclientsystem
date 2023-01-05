51272dca54623ee741cd7caeedefb287e44ad2b3
Freddy Baier <xneerd157@gmail.com>
2023-01-05 10:40:10 -0300

    teste com componente

---
+++ components/table.js
@@ -0,0 +1,75 @@
+const rowsPerPage = 20;
+let currentPage = 1;
+let tableData;
+
+function renderTable() {
+
+    const table = document.getElementById('my-table');
+
+    const startIndex = (currentPage - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+
+    table.innerHTML = '';
+
+    for (let i = startIndex; i < endIndex; i++) {
+        const row = tableData[i];
+        if (!row) break;
+
+        console.log(row)
+
+        const tr = document.createElement('tr');
+
+        tr.id = "linhaTabela"
+
+        tr.setAttribute("onclick", "obterDadosLinhaTabela(event)")
+
+        tr.innerHTML = `
+        <td>${row.id}</td>
+        <td>${row.nome}</td>
+        <td>${row.email}</td>
+        <td>${row.telefone}</td>`
+
+        table.appendChild(tr)
+    }
+}
+
+function prevPage() {
+
+    if (currentPage > 1) {
+        currentPage--
+        renderTable()
+    }
+
+}
+
+function nextPage() {
+
+    if (currentPage < tableData.length / rowsPerPage) {
+        currentPage++;
+        renderTable();
+    }
+
+}
+
+//window.onload = async function() {
+//
+//    startTable()
+//
+//    await fetch('./../data_operations/obterClientes.php').then(Response => Response.json()).then(data => tableData = data)
+//
+//    renderTable()
+//
+//};
+
+async function startTable(linhasPorPagina,arquivoAPI){
+
+    rowsPerPage = linhasPorPagina
+
+    await fetch(arquivoAPI).then(Response => Response.json()).then(data => tableData = data)
+
+    document.getElementById('prev-page').addEventListener('click', prevPage);
+    document.getElementById('next-page').addEventListener('click', nextPage);
+
+    renderTable()
+
+}
