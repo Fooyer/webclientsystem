c508de373df5caaa411fb2699891718f5b6ac455
Freddy Baier <xneerd157@gmail.com>
2023-01-05 13:37:42 -0300

    vamo ve

--- clientes/crud_cliente/index.js
+++ clientes/crud_cliente/index.js
@@ -1,7 +1,7 @@
-import { Table } from "../../components/table"
+const MinhaClasse = require("../../components/table")
 
 window.onload = function() {
 
-    const tabela = new Table(20,'./../data_operations/obterClientes.php')
+    const tabela = new MinhaClasse(20,'./../data_operations/obterClientes.php')
 
 };
