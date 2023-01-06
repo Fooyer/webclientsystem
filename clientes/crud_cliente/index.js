0c32d3ff796262d2db9fbae6e15ffa380fb4c608
Freddy Baier <xneerd157@gmail.com>
2023-01-06 13:16:41 -0300

    time

--- clientes/crud_cliente/index.js
+++ clientes/crud_cliente/index.js
@@ -11,7 +11,7 @@ window.onload = function() {
      setTimeout(function() {
         loadingMessage.parentNode.removeChild(loadingMessage);
         loadingMessage.style.opacity = 0;
-    }, 5000);
+    }, 1000000);
 
 };
 
